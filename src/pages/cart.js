67e6dea33ea83e7e6990b1f27a5ccd654cabf64c
dmytrobusko1106@gmail.com <dmytrobusko1106@gmail.com>
2022-09-09 17:01:30 +0300

    SigmaPractice_DmytroBusko_Project

---
+++ src/pages/cart.js
@@ -0,0 +1,27 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/home";
+import Blog from "./pages/blog";
+import Contact from "./pages/contactus";
+import About from "./pages/aboutus";
+import Cart from "./pages/cart";
+import Shop from "./pages/shop";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
